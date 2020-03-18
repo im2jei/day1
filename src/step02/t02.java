ea9e82130fecbafaa09a7db2d9ccf087d4ca1325
a <a@a-PC>
2020-03-18 19:01:27 +0900

    0318 test

---
+++ src/step02/t02.java
@@ -0,0 +1,18 @@
+package step02;
+
+public class t02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "kim hong gil";
+		int age = 34;
+		char blood = '세';
+		String tel = "010-2222-3333";
+
+		System.out.println("/"+"/"+"다음과 같이 출력을 구하시오");
+		System.out.println("/"+"/"+ name + "(" + age + blood + ")");
+		System.out.println("/"+"/"+tel);
+	}
+
+}
---
+++ testjava.java
@@ -0,0 +1,39 @@
+package member;
+
+public class testjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "홍길동";
+		int age = 32;
+		double point = 132.2;
+		int level = 4;
+		
+		System.out.println("이름:"+name+"("+age+"세"+")");
+		System.out.println("포인트:"+point);
+		System.out.println("레벨:"+level+"등급");
+		
+		System.out.print("이름:");
+		System.out.print("홍길동");
+		System.out.print("(");
+		System.out.print("32세");
+		System.out.println(")");
+		System.out.print("포인트:");
+		System.out.println("132.2");
+		System.out.print("레벨:");
+		System.out.print("4");
+		System.out.print("등급");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
